765b81c807d2726035509b296185c188dffb6920
Ksenzova <ksenzova.an@gmail.com>
2017-02-08 16:52:43 +0300

    First version of working code

---
+++ src/Program/Program/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        /*
+         *  Program that load read options from comand line and print random 3 options
+         *  */
+        const int options_number = 3;
+        static void Main(string[] args)
+        {
+            if (args != null)
+            {
+                Selecter.Selecting(args,options_number);
+            }
+        }
+    }
+}
---
+++ src/Program/Program/Selecter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Program
+{
+    class Selecter
+    {
+        /*
+        * Find  from all options options that will be printed
+        * */
+        public static void Selecting(string[] options_all, int options_number)
+        {
+            Random rand = new Random();
+            /*
+             *  Check number of input options
+             *  If less than two write all options
+             *  If more than two use random to choose
+             * */
+            if (options_all.Length < options_number)
+            {
+                foreach (string s in options_all)
+                    Console.Write(s + " ");  
+            }
+            else
+            {
+                for (int i = 0; i < options_number; i++)
+                {
+                    int selected;
+                    selected = rand.Next(0, options_all.Length);
+                    Console.WriteLine(options_all[selected]);
+                }
+            }
+        }
+    }   
+}
