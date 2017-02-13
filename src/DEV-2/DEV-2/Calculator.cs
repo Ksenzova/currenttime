53ff258669d24d392ca9c23ea84fc165dc9a1009
Ksenzova <ksenzova.an@gmail.com>
2017-02-13 15:00:46 +0300

    modifing files

---
+++ src/DEV-2/DEV-2/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+
+
+namespace DEV_2
+{
+    // Calculate correct mathematical expression
+    class Calculator
+    {
+        // Calculate and then output result
+        public void calculate(string data)
+        {
+            DataTable data_table = new DataTable();
+            var result = data_table.Compute(data, "");
+            Console.WriteLine(result);
+        }
+    }
+}
