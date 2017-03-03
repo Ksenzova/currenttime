a2025dbb168f396347eebbade8daeb94359562bf
Ksenzova <ksenzova.an@gmail.com>
2017-03-03 12:41:34 +0300

    Find in file value by sectiin and key

---
+++ src/DEV-9/DEV-9/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace DEV_9
+{
+    /// <summary>
+    /// Find in File value by section and key
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path =string.Empty;
+            try
+            {
+                path = args[0];
+                bool isExist = File.Exists(path);
+                if(isExist==true)
+                {
+                    Console.WriteLine("Input section");
+                    string section = Console.ReadLine();
+                    section = string.Concat('[', section, ']');
+
+                    Console.WriteLine("Input key");
+                    string key = Console.ReadLine();
+
+                    FileReader fileReader = new FileReader();
+                    string value = fileReader.GetValue(section, key, path);
+                    if(value == string.Empty)
+                    {
+                        Console.WriteLine("There is no value for section{0}, key{1}", section, key);
+                    }
+                    Console.WriteLine(value);
+                }
+                else
+                {
+                    Console.WriteLine("Could not find specified path");
+                }
+            }
+            catch(IndexOutOfRangeException)
+            {
+                Console.WriteLine("There is no input path"); 
+            }    
+               
+            Console.ReadKey();
+        }
+    }
+}
