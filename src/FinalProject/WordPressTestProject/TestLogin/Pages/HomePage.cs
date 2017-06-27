75ba91964d83ae22243f1ef56a0a0d4d0030d8f2
Ksenzova <ksenzova.an@gmail.com>
2017-06-27 19:18:57 +0300

    Add autorization to wordpress

---
+++ src/FinalProject/WordPressTestProject/TestLogin/Pages/HomePage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace FinalProject
+{
+    /// <summary>
+    /// Describe Home page
+    /// </summary>
+    class HomePage
+    {
+        public IWebDriver Driver { get; private set; }
+        protected const string Domain = @"http://localhost:8080/";
+
+        private WebDriverWait wait;
+
+        public HomePage(IWebDriver driver)
+        {
+            this.Driver = driver;
+            driver.Navigate().GoToUrl(Domain);
+        }
+
+        public bool IsHomePage()
+        {
+            bool isHomePage = true;
+            try
+            {
+                wait.Until(ExpectedConditions.ElementIsVisible(
+                    By.XPath(@"//*[@id='welcome-panel']")));
+            }
+            catch (TimeoutException)
+            {
+                isHomePage = false;
+            }
+            return isHomePage;
+        }
+    }
+}
---
+++ src/FinalProject/WordPressTestProject/TestLogin/TestLogin.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FinalProject;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TestLogin.Pages;
+
+namespace TestLogin
+{
+    [TestClass]
+    public class LoginPageTests
+    {
+        HomePage homePage;
+        AuthorizationPage loginPage;
+        string userLogin;
+        string userPassword;
+
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+
+        [DataSource("System.Data.OleDb",
+       "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=\"F:\\AT\\currenttime\\src\\FinalProject\\LoginPassword.mdb\"",
+       "Tb1",
+       DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void LoginPageValidLogin()
+        {
+            userLogin = testContextInstance.DataRow["Login"].ToString();
+            userPassword = testContextInstance.DataRow["Password"].ToString();
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                homePage = new HomePage(driver);
+                loginPage = new AuthorizationPage(driver);
+                loginPage.FillForm(userLogin, userPassword);
+                Assert.IsTrue(homePage.IsHomePage());
+            }
+        }
+
+
+
+        [DataSource("System.Data.OleDb",
+       "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=\"F:\\AT\\currenttime\\src\\FinalProject\\LoginPassword.mdb\"",
+       "Tb2",
+       DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void LoginPageNotValidLogin()
+        {
+            userLogin = testContextInstance.DataRow["Login"].ToString();
+            userPassword = testContextInstance.DataRow["Password"].ToString();
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                homePage = new HomePage(driver);
+                loginPage = new AuthorizationPage(driver);
+                loginPage.FillForm(userLogin, userPassword);
+                Assert.IsFalse(homePage.IsHomePage());
+            }
+        }
+    }
+}
